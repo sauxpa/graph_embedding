20b93c2a9debffc19dae1a06b674108e2d4b06dc
sauxpa <46052865+sauxpa@users.noreply.github.com>
2019-12-29 21:05:32 +0100

    add 2-wasserstein distance

--- graph_embedding_continuity/utils.py
+++ graph_embedding_continuity/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 from copy import deepcopy
+from scipy.spatial.distance import cdist
+from scipy.optimize import linear_sum_assignment
 
 
 def remove_random_edges(G, k=1, in_place=True):
@@ -19,3 +21,21 @@ def remove_random_edges(G, k=1, in_place=True):
         G_.remove_edge(edge[0], edge[1])
 
     return G_
+
+
+def wasserstein_metric(alpha, beta):
+    """Compute 2-Wasserstein distance between point clouds representing
+    empirical measures by solving the linear sum assignment problem in bipartite graphs.
+    alpha, beta: (n,d) arrays:
+        n: number of points in the cloud,
+        d: dimension of the cloud.
+    """
+    
+    if len(alpha.shape) == 1:
+        # cdist needs inputs of shape of length 2
+        dist_matrix = cdist(alpha.reshape(-1,1), beta.reshape(-1,1), metric='euclidean')
+    else:
+        dist_matrix = cdist(alpha, beta, metric='euclidean')
+        
+    assignment = linear_sum_assignment(dist_matrix)
+    return dist_matrix[assignment].sum() / dist_matrix.shape[0]
